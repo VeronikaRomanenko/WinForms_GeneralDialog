3c3e4ca6280b3c0d116f98697469ae8721b1df48
MaksymBondarenko <maksymbondarenko@gmail.com>
2019-04-25 19:16:40 +0300

    Commit1

---
+++ WinForms_GeneralDialog/WinForms_GeneralDialog/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WinForms_GeneralDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(588, 288);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 294);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(150, 44);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "ADD";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(426, 294);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 44);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(179, 294);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(104, 44);
+            this.btnFont.TabIndex = 3;
+            this.btnFont.Text = "FONT";
+            this.btnFont.UseVisualStyleBackColor = true;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(305, 294);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(104, 44);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.Text = "COLOR";
+            this.btnColor.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 350);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnColor;
+    }
+}
+
---
+++ WinForms_GeneralDialog/WinForms_GeneralDialog/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_GeneralDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Client Code
+            btnOpen.Click += BtnOpen_Click;
+            btnSave.Click += BtnSave_Click;
+            btnFont.Click += BtnFont_Click;
+            btnColor.Click += BtnColor_Click;
+            textBox1.ScrollBars = ScrollBars.Vertical;//прокрутка
+            textBox1.WordWrap = true;//перенос слов
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void BtnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.ShowColor = true;
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Font = fontDialog.Font;                
+                if (fontDialog.ShowColor == true)
+                {
+                    textBox1.ForeColor = fontDialog.Color;
+                }
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            InitialFilter(saveFileDialog);
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                File.WriteAllText(fileName, textBox1.Text);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void InitialFilter(FileDialog dialog)
+        {
+            dialog.Filter = "Веб-страница(*.html)|*.html|Text Files(*.txt)|*.txt|C# Code(*.cs)|*.cs|All Files(*.*)|*.*";
+            dialog.FilterIndex = 3;
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            InitialFilter(openFileDialog);
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+    }
+}
